26d1d92764c9d853164bc383466b603873e4b3f0
karlos2023 <karlosluanamica@hotmail.com>
2023-05-02 22:10:41 -0300

    Iphone commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function trocacor (cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+}
+
+function trocacimagem(Image){
+let IPhone = document.querySelector(".IPhone")
+IPhone. src = Image
+} 
